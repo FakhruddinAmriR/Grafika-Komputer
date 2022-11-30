4bdd797025d3e68807e30bfbb29d20303c612624
FakhruddinAmriR <amrifakhruddin@gmail.com>
2022-11-30 21:07:20 +0700

    udah tak tambahin fitur jump tapi kaya e masih salah

--- projek.py
+++ projek.py
@@ -7,9 +7,12 @@ pos_y = 0
 
 mulai = False
 
+chary = 480
+
 def pohon():
 
     #daun
+    glPushMatrix()
     glColor3ub(0, 250, 0)
     glTranslate(0, 0, 0)
     glBegin(GL_POLYGON)
@@ -47,11 +50,13 @@ def pohon():
     glVertex2f(270, 180)
     glVertex2f(270, 100)
     glEnd()
+    glPopMatrix()
 
 def char():
 
+    glPushMatrix()
     glScale(0.2 , 0.2 , 0)
-    glTranslated(0, 480, 0)
+    glTranslated(0, chary, 0)
     glColor3ub(255, 255, 255)
     glBegin(GL_POLYGON)
     glVertex2f(500, 200)
@@ -125,33 +130,36 @@ def char():
     glVertex2f(400, 100)
 
     glEnd()
+    glPopMatrix()
 
 def jalan():
 
     global pos_x, pos_y
+    glPushMatrix()
     glTranslated(pos_x, pos_y, 0)
 
-    pos_x-=5
+    pos_x-=3
     if pos_x <= -5000:
         pos_x = 0
-    glScale(20, 20, 0)
+    glScale(10, 10, 0)
     glTranslated(0, 1, 0)
     glColor3ub(136, 69, 19)
     glBegin(GL_POLYGON)
-    glVertex2f(0, -100)
-    glVertex2f(1000, -100)
-    glVertex2f(1000, 00)
-    glVertex2f(0, 00)
-    glEnd()
+    glVertex2f(0, 10)
+    glVertex2f(1000, 10)
+    glVertex2f(1000, 0)
+    glVertex2f(0, 0)
+    glEnd()   
 
     glTranslated(0, 1, 0)
     glColor3ub(80, 80, 80)
     glBegin(GL_POLYGON)
-    glVertex2f(200, -1)
-    glVertex2f(200, 20)
-    glVertex2f(220, 20)
-    glVertex2f(220, -1)
+    glVertex2f(100, 0)
+    glVertex2f(100, 20)
+    glVertex2f(110, 20)
+    glVertex2f(110, 0)
     glEnd()
+    glPopMatrix()
 
 def tombolmulai():
     glColor3ub(80, 80, 80)
@@ -163,9 +171,19 @@ def tombolmulai():
     glEnd()
 
 def start(key,x,y):
-    global mulai
+    global mulai, chary
     if key == b'\r':
         mulai = True
+    
+    if key == b' ':  
+        # chary+=1000
+
+        if chary <= 1480:
+            chary += 500
+
+grafiti = 4
+
+
 
 
 def iterate():
@@ -177,14 +195,16 @@ def iterate():
     glLoadIdentity()
 
 def showScreen():
-    global mulai
+    global mulai, chary
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
     iterate()
     glutSwapBuffers()
     glClearColor(0, 200, 200, 1)
-    if mulai == True:
+    if chary >= 480:
+        chary -= grafiti
         
+    if mulai == True:  
         pohon()
         char()
         jalan()
